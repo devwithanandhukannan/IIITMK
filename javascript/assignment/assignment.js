e266cd67b37ab6f9c91a8deabf633b9ce5889a0d
Anandhu <connect.anandhukannan@gmail.com>
2025-12-05 12:23:51 +0530

    updated javascript add some cart mgt using map datastructure

--- javascript/assignment/assignment.js
+++ javascript/assignment/assignment.js
@@ -84,3 +84,164 @@ for (let i = 1; i<=n; i++){
     }
 }
 
+
+9.// Write a program that reads one character and prints whether it is:
+// a letter
+// a digit
+// a space
+// or a special character
+console.log("9. Read one character and print a letter digit space or special character")
+let char1 = 'q'
+if(/[a-zA-z]/.test(char1)){
+  console.log(`${char1} is a letter`);
+}else if(/[0-9]/.test(char1)){
+  console.log(`${char1} is a character`);
+}else if(char1 == ' '){
+  console.log(`${char1} is a space`);
+}else{
+  console.log(`${char1} is a special character`);
+}
+
+
+10.//JS program to count how many consonants appear in a string (only letters a–z). Ignore spaces and punctuation.
+
+console.log("10. ignore spaces and punctuation")
+let word = 'sdfafdsfasq23sdfasdf'
+const vowels_ = 'aeiou'
+let count_consonants=0
+for(let i=0;i<word.length-1;i++){
+  let ch = word[i].toLowerCase();
+  if(!vowels_.includes(ch)){
+    count_consonants++
+  }
+}
+console.log(count_consonants);
+
+
+
+11.// Write a program to capitalize first letter in every word of a sentence using string handling functions
+console.log("10. Write a program to capitalize first letter in every word of a sentence using string handling functions");
+
+let input = "hello world this is javascript";
+let data = input.split(" ")
+let data1 = data.map(word=>
+word.charAt(0).toUpperCase()+word.slice(1).toLowerCase()
+    ).join(" ")
+let result_ = data1.toString()
+console.log(result_)
+
+12.//Write a program to read a value “abcd23ef78gh98za” . From this, remove digits and print only the characters
+console.log("12. Write a program to read a value “abcd23ef78gh98za” . From this, remove digits and print only the characters");
+
+let value= 'abcd23ef78gh98za'
+let counter_value=''
+for (let i =0;i<=value.length-1;i++){
+  if(!/[0-9]/.test(value[i])){
+    counter_value+=value[i]
+  }
+}
+console.log(counter_value);
+
+15.//Consider an array [2,3,-4,0,-2,-6,7,8,9]. Count the number of positive,negative and zeros in this array.
+console.log("15. Count the number of positive,negative and zeros");
+let arr = [2,3,-4,0,-2,-6,7,8,9]
+let positive =0
+let negative =0
+let zero =0
+arr.forEach((items)=>{
+  if(items>0){
+    positive++
+  }else if(items<0){
+    negative++
+  }else{
+    zero++
+  }
+})
+console.log(positive, negative, zero)
+
+
+13.//JS program to print prime numbers between 10 and 30
+console.log('13.JS program to print prime numbers between 10 and 30');
+function checkprime(number){
+  is_prime= true
+  if(number<1){
+    is_prime= false
+  }else{
+    for(let i=2;i<number-1;i++){
+      if(number%i==0){
+        is_prime= false;
+        break;
+      }
+    }
+  }
+  return is_prime
+}
+for(let i=10;i<=30;i++){
+  let result = checkprime(i);
+  console.log(`${i} is ${result?'prime number':'not a prime number'}`);
+  
+}
+
+14.//Generate fibonacci series upto 11
+let fibonacci = (number) => {
+  if(number<=1){
+    return number;
+  }
+  return fibonacci(number-1)+fibonacci(number-2)
+}
+
+let terms =11
+for (let i=0;i<11;i++){
+  console.log(fibonacci(i));
+  
+}
+
+16.//JS program to create the pattern
+//1
+//12
+//123
+//1234
+limit = 4
+for(let i = 0; i<limit;i++){
+  line_out =''
+  for (let j=1; j<=i+1;j++){
+    line_out+=j;
+  }
+  console.log(line_out);
+  
+}
+
+limit = 4
+for(let i = 0; i<limit;i++){
+  line_out =''
+  for (let j=limit; j>i;j--){
+    line_out+='*';
+  }
+  console.log(line_out);
+  
+}
+
+limit = 4
+for(let i = 1; i<=limit;i++){
+  line_out =''
+  for (let j=limit; j>=0;j--){
+    line_out+=i;
+  }
+  console.log(line_out);
+  
+}
+
+limit = 4
+ counter =0 
+for(let i = 1; i<=limit+1;i++){
+   
+  line_out =''
+  for (let j=1; j<=i;j++){
+      counter++
+    line_out+=counter+' ';
+  }
+  console.log(line_out);
+  
+}
+
+
