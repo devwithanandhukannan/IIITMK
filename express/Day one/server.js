ea173b0d0f88d8907f21a8a2ff6f84075af058e3
Anandhu <connect.anandhukannan@gmail.com>
2026-01-19 12:11:35 +0530

    day one express

--- express/Day one/server.js
+++ express/Day one/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
-dotenv.config()
 
+dotenv.config()
 const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('this is your homepage data')
+})
+
 const port = process.env.PORT
 app.listen(port,()=>{
     console.log(`server running at ${port}`);
